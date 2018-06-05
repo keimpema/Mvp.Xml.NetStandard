d27781d4783b40f647e665a8e0f7ce3e6636b30d
keimpema <harmen@vankeimpema.net>
2018-06-05 21:11:07 +0200

    Fixed build script.

--- create-buildtools.bat
+++ create-buildtools.bat
@@ -3,7 +3,7 @@
 rem set working folder
 cd %~dp0buildtools\methodrenamer
 
-call dotnet restore
+call dotnet restore -r win-x64
 if not "%errorlevel%"=="0" goto failure
 
 call dotnet publish --configuration Release --framework netcoreapp2.0 -r win-x64 --no-restore --output ..\..\artifacts\buildtools
